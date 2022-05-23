92a105c936514f682d8a852a683122ee93cbd844
valentinb26 <vab.wn@icloud.com>
2022-05-23 16:08:00 +0200

    in inteli geändert

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hallo janne ändert dies falls du es sehen kannst");
         System.out.println("ich mach was");
+        System.out.println("habe was in intelij geändert");
     }
 }
