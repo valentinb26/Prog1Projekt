07d9a6564b7feb8a2103b4535dbd3d5caca1a1f5
valentinb26 <vab.wn@icloud.com>
2022-05-23 16:05:37 +0200

    hab was getestet

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("hallo janne ändert dies falls du es sehen kannst");
+        System.out.println("ich mach was");
     }
 }
