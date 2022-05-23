0cc9da2c0477aaa4c7daf9fe0ed9003994924ee2
Janne Liebig <21lija1bif@hft-stuttgart.de>
2022-05-23 16:08:16 +0200

    Hallo

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("hallo janne ändert dies falls du es sehen kannst");
+        System.out.println("hallo janne aendert dies falls du es sehen kannst :D");
     }
 }
