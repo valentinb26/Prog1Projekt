a3397e4bca364b994a1214cad246df9f5562da4d
Janne Liebig <21lija1bif@hft-stuttgart.de>
2022-05-24 09:18:18 +0200

    Paar Implementationen gemacht

---
+++ Menu.java
@@ -0,0 +1,22 @@
+public class Menu {
+    // Menue-Zeugs
+
+    public static void printMenu() {
+        System.out.println("Menue des Übelst-Coolen-Umfangreichen-Killer-Kalenders fuer HFT Studenten.");
+        System.out.println("( Zur Menuepunktwahl Nummer eingeben )");
+        System.out.println("\t(1) Termin einsehen");
+        System.out.println("\t(2) Termin erstellen");
+        System.out.println("\t(3) Termin suchen");
+        System.out.println("\t(4) Termin bearbeiten");
+        System.out.println("\t(5) Ansicht");
+        System.out.println("\t(6) Hilfe");
+        System.out.println("\t(7) Beenden");
+
+        // Prompt
+        System.out.print("?-");
+
+        // Eingabe
+        // Eingabeverarbeitung
+        // Ausführen
+    }
+}
