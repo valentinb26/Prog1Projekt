ac1c80d464a5720b5c0bcfbb997ed0543c77af78
valentinb26 <vab.wn@icloud.com>
2022-05-25 10:19:55 +0200

    Menu nummer eingabe

--- Menu.java
+++ Menu.java
@@ -23,7 +23,10 @@ public class Menu {
 
     public static void inputHauptMenu(int ui){
         switch (ui) {
-            case 1 -> System.out.println("Von ihnen gewählte Menüpunkt: " + ui+" (Termin einsehen)");
+            case 1 -> {
+                System.out.println("Von ihnen gewählte Menüpunkt: " + ui+" (Termin einsehen)");
+                Verwaltung.termineEinsehen();
+            }
             case 2 -> System.out.println("TEMPERROR nummer: " + ui);
             case 3 -> System.out.println("TEMPERROR nummer: " + ui);
             case 4 -> System.out.println("TEMPERROR nummer: " + ui);
@@ -32,9 +35,5 @@ public class Menu {
             case 7 -> System.out.println("TEMPERROR nummer: " + ui);
             default -> System.out.println("keine gültige Nummer!");
         }
-    }
-
-    public static void inputTerminEinsehen(Termin t){
-        System.out.println(t.toString());
     }
 }
