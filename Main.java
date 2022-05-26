167eba49e81282ad184f2faf2a0a8f6240179d4b
valentinb26 <vab.wn@icloud.com>
2022-05-26 12:33:50 +0200

    --

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //du willst dass man wieder ins menü kommt um andere Dinge zu tun oder ?
         boolean running = true;
         
         while(running) {
