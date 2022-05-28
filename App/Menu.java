63708d25e3c37331f64f5b9bd347047cc847dab9
Janne Liebig <21lija1bif@hft-stuttgart.de>
2022-05-28 15:11:32 +0200

    Menuepunkt "Bearbeiten ergänzt"

--- App/Menu.java
+++ App/Menu.java
@@ -10,14 +10,15 @@ public class Menu {
     public static void printMenu() {
         //Output.clearScreen();
         System.out.println("\n\t\tM E N U E\n");
-        System.out.println("( Zur Menuepunktwahl Nummer eingeben )");
+        //System.out.println("( Zur Menuepunktwahl Nummer eingeben )");
         System.out.println("\t(1) Termine einsehen");
         System.out.println("\t(2) Termin erstellen");
         System.out.println("\t(3) Termin suchen");
-        System.out.println("\t(4) Termin loeschen / eigentlich bearbeiten");
-        System.out.println("\t(5) Ansicht");
-        System.out.println("\t(6) Hilfe");
-        System.out.println("\t(7) Beenden");
+        System.out.println("\t(4) Termin loeschen");
+        System.out.println("\t(5) Termin bearbeiten");
+        System.out.println("\t(6) Ansicht");
+        System.out.println("\t(7) Hilfe");
+        System.out.println("\t(8) Beenden");
     }
 
     public static void inputHauptMenu(){
@@ -40,20 +41,24 @@ public class Menu {
             Verwaltung.terminSuchen();
         }
         case 4 -> {
-            System.out.println(repeat1 + ui + " (Termin bearbeiten)");
+            System.out.println(repeat1 + ui + " (Termin loeschen)");
             System.out.print("ID: ");
             int id = Input.readInt();
             Verwaltung.terminLoeschen(id);
         }
         case 5 -> {
-            System.out.println(repeat1 + ui + " (Ansicht)");
+            System.out.println(repeat1 + ui + " (Termin bearbeiten)");
             //in arbeit
         }
         case 6 -> {
-            System.out.println(repeat1 + ui + " (Hilfe)");
-
+            System.out.println(repeat1 + ui + " (Ansicht)");
+            // Ansicht
         }
         case 7 -> {
+            System.out.println(repeat1 + ui + " (Hilfe)");
+            // Hilfe
+        }
+        case 8 -> {
             System.out.println(repeat1 + ui + " (Beenden)");
             System.out.println("Auf Wiedersehen :)");
             System.exit(0);
