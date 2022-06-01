d1a9067069cf787951c94dc4c238978fc4ffdb7f
valentinb26 <vab.wn@icloud.com>
2022-06-01 10:44:05 +0200

    csv added

--- App/Main.java
+++ App/Main.java
@@ -1,4 +1,5 @@
 package App;
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ OwnUtil/CsvIO.java
@@ -0,0 +1,13 @@
+package OwnUtil;
+
+public class CsvIO {
+    //Klasse für Input und Output in und aus einer Csv Datei
+
+    public static void CsvRead(){
+        System.out.println();
+    }
+
+    public static void CsvWrite(){
+
+    }
+}
