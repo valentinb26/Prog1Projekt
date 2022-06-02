2354feba0df42b4cef8dad0412a2c613f38d4946
Janne Liebig <21lija1bif@hft-stuttgart.de>
2022-06-02 14:40:59 +0200

    -

--- Ansicht/KonsolenAnsicht.java
+++ Ansicht/KonsolenAnsicht.java
@@ -4,21 +4,17 @@ import Typen.Termin;
 import App.Verwaltung;
 import OwnUtil.Output;
 
-public class KonsolenAnsicht extends Ansicht { 
+public class KonsolenAnsicht { 
     // Konsolenansichts-Zeugs.
-    @Override
     public void erstelleFormatierteAnsicht() {
         
     }
-    @Override
-    public void erstelleListenAnsicht() {
+    public static void erstelleErweiterteAnsicht() {
         // irgendwie noch nach Datum und Uhrzeit sortieren
-        Output.printTitle("Kompakte Ansicht");
-        System.out.printf("\n%-10s | %-7s     | %-4s | %s\n", "DATUM", "UHRZEIT", "ID", "BEZEICHNUNG");
-        System.out.println("---------------------------------------------------");
         for(Termin t : Verwaltung.getTermine()) {
-            // DATUM:- UHRZEIT: \tID:- NAME:- 
-            System.out.printf("%-10s | %-7s     | %-4d | %s\n", t.getDatum().toString(), t.getUhrzeit().toString(), t.getID(), t.getName());
+            System.out.println(Output.SEPARATOR);
+            System.out.println(t);
         }
+        System.out.println(Output.SEPARATOR);
     }
 }
